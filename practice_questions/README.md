009b8b62feeecbc13b2890b5f2ac9cef97961a9c
aatifbirdeye <aatif.bandey@birdeye.com>
2017-03-26 19:08:56 +0530

    implement bubble sort example

---
+++ practice_questions/README.md
@@ -0,0 +1,4 @@
+Two of the most common operations performed on data stored in a computer are sorting
+and searching. This has been true since the beginning of the computer industry, so this
+means that sorting and searching are two of the most studied operations in computer
+science
--- practice_questions/arrayQuestions.js
+++ practice_questions/arrayQuestions.js
@@ -20,6 +20,32 @@ Array.prototype.newMap = function(arg){
 no_cars.newMap(multiply);
 console.log(no_cars_new);
 
-//somearray.map(()=>{
-	// () // this is a function which runs inside map
- //})
+//SORTING ALGORITHM......................
+
+
+
+// BUBLE SORT [Ascending array] in one array with module patten example or closure
+// Sort an Array ascending order in IIFE
+var myIife  = (function(){
+	var sort_item = ["4", "2", "5", "1"];
+
+	var sortAscending = function(){
+  		for(var k = 0; k < sort_item.length; k++ ){
+			for(var m = 0; m < sort_item.length; m++){
+				if(sort_item[m] > sort_item[m + 1]){
+				var temp = sort_item[m];  // 4
+				sort_item[m] = sort_item[m + 1]; // 2
+				sort_item[m+1] = temp;
+				}
+				
+			}
+		}
+      	return sort_item;
+	}
+    return {
+      test : sortAscending
+    }
+})()
+console.log(myIife.test()); // [1,2,4,5]
+
+
